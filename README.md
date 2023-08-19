24b55195772ab99101452eaad20b61bafb15c842
R-Otsuka <yakugozy@gmail.com>
2023-08-19 15:07:12 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+webpack: 本体。単体だとコマンドラインからの利用はできない
+webpack-cli: コマンドラインで、webpackを実行するためのライブラリ
+ts-loader: webpack で typescript を処理するためのオプション
---
+++ ts/index.ts
@@ -0,0 +1,13 @@
+// 1-1
+type hw = 'hello world';
+const message: hw = 'hello world';
+console.log(message);
+
+
+// 1-2
+interface Todo {
+    title: string;
+    description: string;
+}
+
+const 
